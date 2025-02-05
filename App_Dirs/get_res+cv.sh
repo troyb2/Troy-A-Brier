cbe3e8455d027fedbb312cc0c4e894816538634e
troyb2 <troyb2@illinois.edu>
2025-02-05 13:45:45 -0500

    updating cover letter, making wistar app dir

--- App_Dirs/get_res+cv.sh
+++ App_Dirs/get_res+cv.sh
@@ -1,3 +1,8 @@
-cp ../RESUME/resume_tabrier.pdf ./
+dir=$1
 
-cp ../CV/cv_tabrier.pdf ./
+cp ../RESUME/resume_tabrier.pdf ./$dir/resume.pdf
+
+cp ../CV/cv_tabrier.pdf ./$dir/cv.pdf
+
+cp ../Cover_Letter/cover_letter.tex ./$dir/letter.tex
+cp ../Cover_Letter/cover_letter.pdf ./$dir/letter.pdf
