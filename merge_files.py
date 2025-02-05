650c26bedf9a95f59b4f4161714872eaad9b8573
troyb2 <troyb2@illinois.edu>
2025-02-05 15:11:24 -0500

    adding submission for Synethsize.Bio

---
+++ merge_files.py
@@ -0,0 +1,35 @@
+from PyPDF2 import PdfMerger
+import argparse
+import glob
+
+
+parser = argparse.ArgumentParser(description="Quick PDF Merger")
+
+parser.add_argument('-d', '--in_dir')   
+parser.add_argument('-o', '--out_file')
+
+args = parser.parse_args()
+
+outfile = args.out_file
+
+print('#####################################################################')
+print(outfile)
+print(args.in_dir)
+print('#####################################################################')
+
+file_list = glob.glob(str(args.in_dir)+'*')
+
+
+file_list = ['letter.pdf','resume.pdf']
+merger = PdfMerger()
+
+for f in file_list:
+	print('adding',f)
+	#Merge Individual PDF files
+	merger.append(f)
+#	print('adding','norm-'+f)
+#	merger.append('norm-'+f)
+
+merger.write(outfile)
+merger.close()
+print("Generated "+outfile)
